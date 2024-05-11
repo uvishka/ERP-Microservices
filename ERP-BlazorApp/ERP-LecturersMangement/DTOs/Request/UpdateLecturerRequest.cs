a4e068b16625acbf49a3bfb22489864356f15cbd
Senevirathna12 <anishantha702@gmail.com>
2024-05-11 17:40:13 +0530

    FrontEnd-LecturerManagement

---
+++ ERP-BlazorApp/ERP-LecturersMangement/DTOs/Request/UpdateLecturerRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_LecturersMangement.DTOs.Request
+{
+    public class UpdateLecturerRequest
+    {
+        public string LecturerID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Department { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+        public string NationalId { get; set; } = string.Empty;
+
+        public DateTime DoB { get; set; }
+
+        // public virtual ICollection<Student>? Advicees { get; set; }
+    }
+}
